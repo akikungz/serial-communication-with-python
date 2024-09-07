09ad766f71ec1da97acec639e97a6b01b140887e
Thitipong Tapianthong <thitipong.t@email.kmutnb.ac.th>
2024-09-08 01:52:18 +0700

    Refactor serial communication and add input thread
    This commit refactors the serial communication code in `main.py` to allow the user to specify the port and baudrate. It also adds a separate input thread to handle user input while listening for serial data. This improves the overall functionality and user experience of the program.

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from serial import Serial
+from threading import Thread
 
-serial = Serial('/dev/ttyUSB0', 9600)
+port = input("Enter the port (Example: COM3, /dev/ttyUSB0): ")
+bund = input("Enter the baudrate (Example: 9600, 115200): ")
 
-while True:
-    data = serial.readline()
-    print(data)
+if (port == "" or bund == ""):
+    print("You must enter a port and a baudrate.")
+    exit()
+
+if (not bund.isdigit()):
+    print("The baudrate must be a number.")
+    exit()
+
+# Serial input thread
+def serial_input():
+    while True:
+        ser.write(input().encode("utf-8") + b"\n")
+
+with Serial(port, int(bund)) as ser:
+    # Start the serial input thread
+    input_thread = Thread(target=serial_input)
     
-    if data == b'exit\n':
-        break
-
-serial.close()
+    while True:
+        input_thread.start()
+        if ser.in_waiting > 0:
+            print(ser.readline().decode("utf-8").strip())
+            # Destroy the serial input thread
+            input_thread.join()
